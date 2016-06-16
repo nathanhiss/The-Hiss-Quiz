386f79a614c6172529e6e70ebdec41a6b7aca413
Nathan Hiss <hissnathan@gmail.com>
2016-06-16 13:43:39 +0000

    Updated styles and answer text.

--- client/js/base.js
+++ client/js/base.js
@@ -1,41 +1,48 @@
 function MyController($scope, $http) {
-    
+
     $scope.questions = [];
     $scope.currentsquestion = {};
-    $scope.color= '';
     $scope.message = '';
-     $scope.done = false;
-   var questionIndex = 0;
+    $scope.done = false;
+    var questionIndex = 0;
     var answers = [];
-    
+
     $scope.postData = function() {
-        $http.post('/api/v1/postData', {color: $scope.color}).then(function(response) {
-            $scope.message = response.data.message;
+        $http.post('/api/v1/postData', {
+            answers: answers
+        }).then(function(response) {
+            console.log(response.data.message);
+            $scope.message = response.data.message.text;
+            $scope.img = response.data.message.img;
         });
-        $scope.color = '';
+        $scope.answers = '';
     };
-    
+
     $scope.getData = function() {
         $http.get('/api/v1/getData').then(function(response) {
             $scope.questions = response.data;
-  $scope.currentQuestion =  $scope.questions[questionIndex];
+            $scope.done = false;
+            questionIndex = 0;
+            $scope.currentQuestion = $scope.questions[questionIndex];
             console.log($scope.currentQuestion);
         });
     };
-    
+
     $scope.answerQuestion = function(answerValue) {
-        console.log(answerValue); console.log(questionIndex);
+        console.log(answerValue);
+        console.log(questionIndex);
         answers.push(answerValue);
         questionIndex = questionIndex + 1
-         console.log('questionIndex: ' + questionIndex);
-       console.log('currentQuestion: ' + JSON.stringify($scope.questions[questionIndex]));
+        console.log('questionIndex: ' + questionIndex);
+        console.log('currentQuestion: ' + JSON.stringify($scope.questions[questionIndex]));
         $scope.currentQuestion = $scope.questions[questionIndex]
-        if (questionIndex == $scope.questions.length ){
+            //checking if the quiz is over!
+        if (questionIndex == $scope.questions.length) {
             $scope.done = true;
-            questionIndex = 0;
-            console.log( 'Wait a Moment we are Building a Wall');
+            console.log('Wait a Moment we are Building a Wall');
+            $scope.postData();
         }
-            
+
     };
 }
-//record their data and based on a certain amout of answers they give they will get an output too their answers which they gave. 
+//record their data and based on a certain amout of answers they give they will get an output too their answers which they gave.
